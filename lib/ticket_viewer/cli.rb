4b775e639413eec3bd95912d7034c598865aa18d
Alex Mercea <alex@lxmrc.com>
2021-05-15 19:41:37 +1000

    Add 'auth' command

--- lib/ticket_viewer/cli.rb
+++ lib/ticket_viewer/cli.rb
@@ -1,6 +1,24 @@
 require "thor"
+require "netrc"
 
 module TicketViewer
   class CLI < Thor
+    desc "auth", "Configure Zendesk API credentials"
+    def auth
+      netrc = Netrc.read
+      netrc["lxmrc.zendesk.com"] = [prompt_username, prompt_password]
+      netrc.save
+      puts "\nLogin saved."
+    end
+
+    private
+
+    def prompt_username
+      ask("Username:")
+    end
+
+    def prompt_password
+      ask("Password:", echo: false)
+    end
   end
 end
--- ticket_viewer.gemspec
+++ ticket_viewer.gemspec
@@ -31,4 +31,5 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "thor", "~> 1.0"
   spec.add_dependency "httparty", "~> 0.18.1"
+  spec.add_dependency "netrc", "~> 0.11.0"
 end
