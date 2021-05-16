ea7e3cafd7ca55e215f6bf3ee587d5a6fb554653
Alex Mercea <alex@lxmrc.com>
2021-05-16 11:18:17 +1000

    Add parsing pages of tickets

--- lib/ticket_viewer/parser.rb
+++ lib/ticket_viewer/parser.rb
@@ -2,9 +2,17 @@ module TicketViewer
   class Parser
     class << self
       def parse_ticket(json)
-        hash = parse_json(json)[:ticket]
-        hash = hash.slice(:id, :subject, :description, :requester_id, :created_at)
-        Ticket.new(hash)
+        ticket_hash = parse_json(json)[:ticket]
+        ticket_hash = ticket_hash.slice(:id, :subject, :description, :requester_id, :created_at)
+        Ticket.new(ticket_hash)
+      end
+
+      def parse_page(json)
+        ticket_hashes = parse_json(json)[:tickets]
+        ticket_hashes.map do |ticket_hash|
+          ticket_hash = ticket_hash.slice(:id, :subject, :description, :requester_id, :created_at)
+          Ticket.new(ticket_hash)
+        end
       end
 
       private
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe TicketViewer::Parser do
   subject(:parser) { described_class }
 
-  let(:tickets_json) { File.open("./spec/fixtures/page_1.json").read }
+  let(:page_json) { File.open("./spec/fixtures/page_1.json").read }
   let(:ticket_json) { File.open("./spec/fixtures/ticket.json").read }
 
   it "parses one ticket" do
@@ -12,5 +12,14 @@ RSpec.describe TicketViewer::Parser do
     expect(ticket.subject).to eq("velit eiusmod reprehenderit officia cupidatat")
     expect(ticket.description).to match("Aute ex sunt culpa ex ea esse sint cupidatat")
     expect(ticket.requester_id).to eq(902093905946)
+  end
+
+  it "parses a page of tickets" do
+    page = parser.parse_page(page_json)
+    expect(page.size).to eq(25)
+    expect(page.first.id).to eq(209)
+    expect(page.first.subject).to eq("velit eiusmod reprehenderit officia cupidatat")
+    expect(page.first.description).to match("Aute ex sunt culpa ex ea esse sint cupidatat")
+    expect(page.first.requester_id).to eq(902093905946)
   end
 end
